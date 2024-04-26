3c1693a2503af8a2c08b4f77fb39629c2a02d7a8
joelgerge-web <joelgeorge98765@gmail.com>
2024-04-26 21:06:56 +0530

    upper right triangle added

--- triangle_ri.py
+++ triangle_ri.py
@@ -2,6 +2,6 @@ while True:
     n = int(input("enter the number of rows: "))
 
     for i in range(n):
-        print(" "*(n-i-1), end="")
+        print(" "*(n-i-1), end=" ")
         print("*"*(i+1))
     break
