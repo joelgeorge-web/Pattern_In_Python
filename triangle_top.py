5a2ee1ff9d55458f26714bc591666577a485b9e0
joelgerge-web <joelgeorge98765@gmail.com>
2024-04-26 20:54:15 +0530

    Refactor triangle_top.py to print a triangle pattern

--- triangle_top.py
+++ triangle_top.py
@@ -1,6 +1,6 @@
 while True:
     n = int(input("Enter the number of rows: "))
     for i in range(n):
-        for j in range(i):
-            print("*", end=" ")
-        print("\n")
+        for j in range(i, n) :
+            print("*", end="")
+        print()
